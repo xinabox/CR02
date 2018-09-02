246472ac09eb3789a924f8c4948aef491dea535a
brendanvanbreda <brendanvanbreda1993@gmail.com>
2018-09-02 23:29:57 +0200

    add examples

---
+++ examples/CR02_client/CR02_client.ino
@@ -0,0 +1,71 @@
+#include <RH_RF95.h> // 
+
+#define LED_BUILTIN 16
+
+#define CR02_FREQUENCY 868.0
+
+uint8_t tempdata[30];
+
+RH_RF95 CR02;
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(115200);
+
+  if (!CR02.init()){
+    Serial.println("init failed");
+  }
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
+  // you can set transmitter powers from 5 to 23 dBm:
+  // Failure to do that will result in extremely low transmit powers.
+
+  //CR02.setModemConfig(CR02.Bw31_25Cr48Sf512);
+  CR02.setFrequency(CR02_FREQUENCY);
+  CR02.setTxPower(23, false);
+}
+
+void loop()
+{
+  Serial.println("Sending to CR03_server");
+  // Send a message to CR03_server
+
+  sprintf(tempdata, "%s", "Hello Server");
+
+  CR02.send(tempdata, sizeof(tempdata));
+
+  CR02.waitPacketSent();
+  // Now wait for a reply
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+
+  if (CR02.waitAvailableTimeout(3000))
+  {
+    // Should be a reply message for us now
+    if (CR02.recv(buf, &len))
+    {
+      digitalWrite(LED_BUILTIN, HIGH);
+      Serial.print("got reply: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(CR02.lastRssi(), DEC);
+    }
+    else
+    {
+      Serial.println("recv failed");
+    }
+  }
+  else
+  {
+    Serial.println("No reply, is CR03_server running?");
+  }
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(400);
+}
+
+
---
+++ examples/CR02_server/CR02_server.ino
@@ -0,0 +1,59 @@
+#include <RH_RF95.h>
+
+#define LED_BUILTIN 16
+
+#define CR02_FREQUENCY 868.0
+
+uint8_t tempdata[30];
+
+RH_RF95 CR02;
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(115200);
+
+  if (!CR02.init()) {
+    Serial.println("init failed");
+  }
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
+  // you can set transmitter powers from 5 to 23 dBm:
+  // Failure to do that will result in extremely low transmit powers.
+
+  //CR02.setModemConfig(CR02.Bw31_25Cr48Sf512);
+  CR02.setFrequency(433.0);
+  CR02.setTxPower(23, false);
+}
+
+void loop()
+{
+  if (CR02.available())
+  {
+    // Should be a message for us now
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+    if (CR02.recv(buf, &len))
+    {
+      digitalWrite(LED_BUILTIN, HIGH);
+      Serial.print("got request: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(CR02.lastRssi(), DEC);
+
+      // Send a reply
+      sprintf(tempdata, "%s", "Hello Client");
+      CR02.send(tempdata, sizeof(tempdata));
+      CR02.waitPacketSent();
+      Serial.println("Sent a reply");
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+    else
+    {
+      Serial.println("recv failed");
+    }
+  }
+}
+
+
